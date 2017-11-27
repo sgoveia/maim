55f445195180c4dff7aebd3eca486bbc6e9e50be
stephen.goveia <stephen.goveia@fireeye.com>
2017-11-27 18:36:33 -0500

    intial commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+?>
+<!DOCTYPE HTML>
+<!--
+	
+-->
+<html>
+	<head>
+		<title>mountainAI Comming Soon!!!</title>
+		<meta charset="utf-8" />
+		<link rel="icon" href="favicon.ico" type="image/x-icon">
+		<meta name="description" content='an Artificial Intelligence company' />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="loading">
+		<div id="wrapper">
+			<div id="bg"></div>
+			<div id="overlay"></div>
+			<div id="main">
+
+				<!-- Header -->
+					<header id="header">
+	
+						<h1>mountain<span style="color:#0EBFE9">A</span>I</h1>
+						<h2>an Artificial Intelligence company</h2>
+						
+						<p>mountainAI is an Artificial Intelligence company focused on practical application.</p>
+						<p>We are developing interfaces and models to apply general and narrow AI in practical B2B and B2C applications.</p>
+						<p>Comming Soon!!</p>
+						<nav>
+							<ul>
+								<li><a href="https://twitter.com/?lang=en" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+								<li><a href="https://www.facebook.com" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+								<li><a href="https://www.linkedin.com/" class="icon fa-linkedin"><span class="label">Facebook</span></a></li>
+								<!-- <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>  -->
+								
+							</ul>
+						
+						</nav>
+					</header>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<!--<span class="copyright">&copy; Untitled. Design: <a href="http://html5up.net">HTML5 UP</a>.</span>-->
+						<span class="copyright">&copy;2017 mountain<span style="color:#0EBFE9">A</span>I</span>
+					</footer>
+
+			</div>
+		</div>
+		<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+		<script>
+			window.onload = function() { document.body.className = ''; }
+			window.ontouchmove = function() { return false; }
+			window.onorientationchange = function() { document.body.scrollTop = 0; }
+		</script>
+	</body>
+</html>
